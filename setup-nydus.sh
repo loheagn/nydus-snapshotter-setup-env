f6ed49e49a9a3d9aa46fc563177e747b9cda0b06
loheagn <loheagn@icloud.com>
2023-07-02 23:28:53 +0800

    Initial commit
    Signed-off-by: loheagn <loheagn@icloud.com>

---
+++ setup-nydus.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd /tmp && git clone https://github.com/containerd/nydus-snapshotter.git
+cd nydus-snapshotter && make && mv bin/containerd-nydus-grpc /usr/bin/containerd-nydus-grpc
+cd /tmp
+rm -rf nydus-snapshotter
+NYDUS_VER=v$(curl -s "https://api.github.com/repos/dragonflyoss/image-service/releases/latest" | jq -r .tag_name | sed 's/^v//')
+wget https://github.com/dragonflyoss/image-service/releases/download/$NYDUS_VER/nydus-static-$NYDUS_VER-linux-amd64.tgz
+tar xzvf nydus-static-$NYDUS_VER-linux-amd64.tgz
+mv nydus-static/nydus-image nydus-static/nydusd nydus-static/nydusify /usr/bin/
+rm -rf nydus-static nydus-static-$NYDUS_VER-linux-amd64.tgz
+
+tee -a /etc/containerd/config.toml > /dev/null <<EOT
+[proxy_plugins]
+[proxy_plugins.nydus]
+address = "/run/containerd-nydus/containerd-nydus-grpc.sock"
+type = "snapshot"
+EOT
+
+systemctl restart containerd
---
+++ setup.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+instance_name=nydus-dev
+
+limactl start --name ${instance_name} vm.yaml
+
+limactl shell ${instance_name} sudo sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
+limactl shell ${instance_name} sudo sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+limactl shell ${instance_name} sudo bash -c 'apt-get update -y && apt-get install -y libbtrfs-dev libseccomp-dev sudo psmisc jq lsof net-tools vim neofetch git make wget build-essential'
+
+limactl shell ${instance_name} git config --global user.email "loheagn@icloud.com"
+limactl shell ${instance_name} git config --global user.name "Nan Li"
+limactl shell ${instance_name} git config --global core.editor vim
+limactl shell ${instance_name} git config --global pull.rebase true
+
+limactl shell ${instance_name} mkdir -p /tmp/bin
+limactl copy setup-nydus.sh ${instance_name}:/tmp/bin/setup-nydus.sh
+limactl copy setup-go.sh ${instance_name}:/tmp/bin/setup-go.sh
+limactl shell ${instance_name} sudo bash -c 'chmod +x /tmp/bin/* && cp /tmp/bin/* /usr/local/bin/'
+limactl shell ${instance_name} sudo /usr/local/bin/setup-nydus.sh
+limactl shell ${instance_name} sudo /usr/local/bin/setup-go.sh
+
