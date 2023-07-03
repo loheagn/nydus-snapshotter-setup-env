aa89953237a274be1e6eb2380e9673cc26ff5946
Nan Li <loheagn@icloud.com>
2023-07-03 19:59:40 +0800

    Update setup-nydus.sh

--- setup-nydus.sh
+++ setup-nydus.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export PATH=$PATH:/usr/local/go/bin
 cd /tmp && git clone https://github.com/containerd/nydus-snapshotter.git
 cd nydus-snapshotter && make && mv bin/containerd-nydus-grpc /usr/bin/containerd-nydus-grpc
 cd /tmp
