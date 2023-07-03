fea47d50d87384dcd8c9498ce68e4fe8eb59ed32
Nan Li <loheagn@icloud.com>
2023-07-03 22:00:11 +0800

    Update setup-nydus.sh

--- setup-nydus.sh
+++ setup-nydus.sh
@@ -4,10 +4,10 @@ cd /tmp && git clone https://github.com/containerd/nydus-snapshotter.git
 cd nydus-snapshotter && make && mv bin/containerd-nydus-grpc /usr/bin/containerd-nydus-grpc
 cd /tmp
 rm -rf nydus-snapshotter
-NYDUS_VER=v$(curl -s "https://api.github.com/repos/dragonflyoss/image-service/releases/latest" | jq -r '.tag_name' | sed 's/^v//')
+NYDUS_VER=v2.2.1
 wget https://github.com/dragonflyoss/image-service/releases/download/$NYDUS_VER/nydus-static-$NYDUS_VER-linux-amd64.tgz
 tar xzvf nydus-static-$NYDUS_VER-linux-amd64.tgz
-mv nydus-static/nydus-image nydus-static/nydusd nydus-static/nydusify /usr/bin/
+mv nydus-static/* /usr/local/bin/
 rm -rf nydus-static nydus-static-$NYDUS_VER-linux-amd64.tgz
 
 tee -a /etc/containerd/config.toml > /dev/null <<EOT
