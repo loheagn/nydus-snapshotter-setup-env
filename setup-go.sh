d06015cd1973461be5308c3154773e1b5b90d51c
Nan Li <loheagn@icloud.com>
2023-07-03 19:58:49 +0800

    Update setup-go.sh

--- setup-go.sh
+++ setup-go.sh
@@ -3,3 +3,5 @@
 cd /tmp && wget https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf /tmp/go1.20.5.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile
+echo 'go env -w GO111MODULE=on' >> /etc/profile
+echo 'go env -w  GOPROXY=https://goproxy.cn,direct' >> /etc/profile
